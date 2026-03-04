1931d529436913b12a2a7eb170d430b828fd3648
MadhanRJ03 <madhankumarrj6@gmail.com>
2026-03-04 20:21:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -260,3 +260,19 @@ finalBtn.addEventListener("click", function () {
 
     envelope.addEventListener("click", showLetter);
 });
+
+
+function createHearts(){
+    for(let i=0;i<40;i++){
+        const heart = document.createElement("div");
+        heart.innerHTML = "💙";
+        heart.className = "floating-heart";
+
+        heart.style.left = Math.random()*100+"vw";
+        heart.style.animationDuration = (3 + Math.random()*3) + "s";
+
+        document.body.appendChild(heart);
+
+        setTimeout(()=>heart.remove(),6000);
+    }
+}
